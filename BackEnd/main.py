29ca9c33b81a69acd84fbacd82ce598c1fe958c5
ayushzoc <ayush168662@gmail.com>
2023-03-29 23:23:58 -0500

    End of 29th commit

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,7 +1,10 @@
 from wsgiref import simple_server
 from flask import Flask, request, render_template
 from flask import Response
+import pandas as pd
+import os
 from flask_cors import CORS, cross_origin
+from apps.core.config import Config
 
 app = Flask(__name__)
 CORS(app)
@@ -9,8 +12,25 @@ CORS(app)
 @app.route('/training', methods = ['POST'])
 @cross_origin()
 def training_route_client():
+    """
+    * Method: training_route_client
+    * Description: method to call training route
+    :return: none
+    *
+    * who       when        version     change (include bug = if apply)
+    * -----     -------     -------     -------------------------------
+    * Ray       03/29/2023  1.0         Initial Creation
+    *
+    * Parameters:
+    *   none
+    """
     try:
-        return Response("Training successful!")
+        config = Config()
+        # Get Run ID
+        run_id = config.get_run_id()
+        data_path = config.training_data_path
+        # trainmodel object initialization
+        return Response("Training successful! and it's RunID is: " + str(run_id))
     except ValueError:
         return Response("Error Occurred! %s" % ValueError)
     except KeyError:
